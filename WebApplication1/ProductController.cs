d2d391907ba59a4edfc14827846408ed4a21b63d
qalib <qalib@DESKTOP-48U1CLO>
2024-05-16 12:30:16 +0400

    Add project files.

---
+++ WebApplication1/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ProductController : Controller
+{
+    private static List<Product> products = new List<Product>();
+
+    public IActionResult GetAll()
+    {
+        return View(products);
+    }
+
+    public IActionResult GetById(int id)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+        return View(product);
+    }
+
+    public IActionResult GetByPrice(decimal price)
+    {
+        var filteredProducts = products.Where(p => p.Price == price).ToList();
+        return View(filteredProducts);
+    }
+
+    public IActionResult Add()
+    {
+        return View(new Product());
+    }
+
+    [HttpPost]
+    public IActionResult Add(Product product)
+    {
+        products.Add(product);
+        return RedirectToAction("GetAll");
+    }
+
+    public IActionResult Remove(int id)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+        if (product != null)
+        {
+            products.Remove(product);
+        }
+        return RedirectToAction("GetAll");
+    }
+}
---
+++ WebApplication1/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
